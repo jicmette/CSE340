54f3b1f3dc510e628aa266b20b1de2ca2165d49c
Israel Carmona <jicmettesc@gmail.com>
2025-03-14 22:26:57 -0600

    W02 Assignment, database backup and database queries.

---
+++ database/assignment2.sql
@@ -0,0 +1,31 @@
+INSERT INTO account(
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES(
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+DELETE FROM account
+WHERE account_id = 1;
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'the small interiors',
+        'a huge interior'
+    );
+SELECT inv_make,
+    inv_model,
+    classification_name
+FROM inventory
+    INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification_name = 'Sport';
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/');
